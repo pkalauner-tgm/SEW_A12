35a6c8f04acebc430223a1739496fb8a0347110f
Paul Kalauner <pkalauner@student.tgm.ac.at>
2014-02-12 16:50:03 +0100

    MonoalphabeticCipher

--- A12/src/hambergerkalaunerorosz/MonoalphabeticCipher.java
+++ A12/src/hambergerkalaunerorosz/MonoalphabeticCipher.java
@@ -1,26 +1,61 @@
 package hambergerkalaunerorosz;
 
-public class MonoalphabeticCipher implements Cipher{
+/**
+ * @author Paul Kalauner 3AHIT
+ * 
+ */
+public class MonoalphabeticCipher implements Cipher {
+	private String originalAlhabet = "abcdefghijklmnopqrstuvwxyzäöüß";
 	private String secretAlphabet;
-	
+
+	/**
+	 * Default Konstruktor Geheimalphabet wird auf das Ausgangsalphabet gesetzt
+	 */
 	public MonoalphabeticCipher() {
-		
+		this.secretAlphabet = "abcdefghijklmnopqrstuvwxyzäöüß";
 	}
-	
+
+	/**
+	 * Liefert das Geheimalphabet zurück
+	 * 
+	 * @return secretAlphabet
+	 */
 	public String getSecretAlphabet() {
 		return secretAlphabet;
 	}
-	
+
+	/**
+	 * @see hambergerkalaunerorosz.Cipher
+	 */
 	@Override
 	public String encrypt(String text) {
-		// TODO Auto-generated method stub
-		return null;
+		text.toLowerCase();
+		char[] arrText = text.toCharArray();
+		char[] arrOrrAlp = originalAlhabet.toCharArray();
+		char[] arrSecretAlp = this.secretAlphabet.toCharArray();
+		
+		for (int i = 0; i < arrText.length; i++) {
+			
+			if (arrText[i] > 'a' && arrText[i] < 'z') {
+				System.out.println(arrText[i] - 'a');
+				arrText[i] = arrSecretAlp[arrText[i] - 'a'];
+			}
+		}
+		return new String(arrText);
 	}
 
+	/**
+	 * @see hambergerkalaunerorosz.Cipher
+	 */
 	@Override
 	public String decrypt(String text) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public static void main(String[] args) {
+		MonoalphabeticCipher cipher = new MonoalphabeticCipher();
+		System.out.println(cipher.encrypt("Test"));
 	}
 
 }
