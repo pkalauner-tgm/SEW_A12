2ef4cb8d22214f2f0a2bf1a05a3fa3cac3286c5a
Paul Kalauner <pkalauner@student.tgm.ac.at>
2014-02-14 09:03:22 +0100

    fixed gui bug

--- A12/src/hambergerkalaunerorosz/View.java
+++ A12/src/hambergerkalaunerorosz/View.java
@@ -1,26 +1,29 @@
 package hambergerkalaunerorosz;
 
-/**
- * View Klasse zu A12
- * @author	Philip Orosz
- * @version	20140212
- */
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+/**
+ * View Klasse zu A12
+ * 
+ * @author Philip Orosz
+ * @version 20140212
+ */
 public class View extends JFrame {
-	Control c;
+	private Control c;
 
 	private JPanel buttons;
 	private JPanel center;
-	private JPanel screen;
+	private JPanel north;
 
 	private JButton encrypt;
 	private JButton decrypt;
@@ -31,7 +34,8 @@ public class View extends JFrame {
 	private JTextArea output;
 
 	private JComboBox<String> optionlist;
-	private String[] options;
+	
+	private JLabel type;
 
 	/**
 	 * Konstruktor der View Klasse
@@ -39,58 +43,57 @@ public class View extends JFrame {
 	 * @param c
 	 *            Objekt der Control Klasse
 	 */
-	
-	public View(Control c){
+
+	public View(Control c) {
 		this.c = c;
-		
+
 		this.setSize(800, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
 		this.setTitle("SuperCipher");
-		
+
+		this.north = new JPanel();
 		this.keyword = new JTextField();
+		this.keyword.setColumns(30);
+		this.type = new JLabel("Geheimalphabet: ");
 		
+		this.north.add(type);
+		this.north.add(keyword);
+
 		this.input = new JTextArea();
 		this.output = new JTextArea();
-		
-		this.options = new String[4];
-		this.options[0] = "Substition"; 
-		this.options[1] = "Shift";
-		this.options[2] = "Monoalphabetic";
-		this.options[3] = "Keyword";
-		
+
+		this.input.setWrapStyleWord(true);
+		this.input.setLineWrap(true);
+
+		this.output.setEditable(false);
+		this.output.setBackground(Color.LIGHT_GRAY);
+		this.output.setLineWrap(true);
+		this.output.setWrapStyleWord(true);
+
+		String[] options = { "Substitution", "Shift", "Keyword" };
 		this.optionlist = new JComboBox<String>(options);
-		
-		this.encrypt = new JButton("Verschluesseln");
+
+		this.encrypt = new JButton("Verschlüsseln");
 		this.decrypt = new JButton("Entschlüsseln");
-		
+
 		this.buttons = new JPanel();
-		this.buttons.setLayout(new GridLayout(0,2));
+		this.buttons.setLayout(new GridLayout(0, 3));
 		this.buttons.add(encrypt);
+		this.buttons.add(optionlist);
 		this.buttons.add(decrypt);
-		
+
 		this.center = new JPanel();
-		this.center.setLayout(new BorderLayout());
-		this.center.add(keyword, BorderLayout.NORTH);
-		this.center.add(optionlist, BorderLayout.CENTER);
-		
-		this.screen = new JPanel();
-		this.screen.setLayout(new GridLayout(0,3));
-		this.screen.add(input);
-		this.screen.add(center);
-		this.screen.add(output);
-		
+		this.center.setLayout(new GridLayout(1, 2));
+		this.center.add(input);
+		this.center.add(output);
+
+		this.add(north, BorderLayout.NORTH);
+		this.add(center, BorderLayout.CENTER);
 		this.add(buttons, BorderLayout.SOUTH);
-		this.add(screen, BorderLayout.CENTER);
-	
-		
+
 		this.setVisible(true);
-		
-		
-		
-		
-		
-		
+
 	}
-	
+
 }
