7bbf1fc2d483fd79c24e120d67706a03f5e1d63c
Philip Orosz <porosz@student.tgm.ac.at>
2014-02-19 16:40:05 +0100

    added javadoc

--- A12/src/hambergerkalaunerorosz/test/TestKeywordCipher.java
+++ A12/src/hambergerkalaunerorosz/test/TestKeywordCipher.java
@@ -10,26 +10,46 @@ import org.junit.Test;
  * @author Philip Orosz
  *
  */
-@SuppressWarnings("javadoc")
+
 public class TestKeywordCipher {
 
+	/**
+	 * Testet den Konstruktor
+	 */
 	@Test
 	public void testKeywordCipher() {
 		KeywordCipher kc = new KeywordCipher("hund");
 		assertEquals("hundabcefgijklmopqrstvwxyzäöüß", kc.getSecretAlphabet());
 	}
 
+	/**
+	 * Testet die Exception falls ein mehr als 30-stelliges keyword angegeben wurde
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testException(){
 		new KeywordCipher("dieseskeywordistgrößeralsdreissigzeichen");
 	}
 	
+	/**
+	 * Testet die Verschlüsselungsmethode
+	 * 
+	 * eingabe: affe
+	 * keyword: hund
+	 * erwartete ausgabe: hbba
+	 */
 	@Test
 	public void testEncrypt() {
 		KeywordCipher kc = new KeywordCipher("hund");
 		assertEquals("hbba", kc.encrypt("affe"));
 	}
 	
+	/**
+	 * Testet die Entschlüsselungsmethode
+	 * 
+	 * eingabe: hbba
+	 * keyword: hund
+	 * erwartete ausgabe: affe
+	 */
 	@Test
 	public void testDecrypt() {
 		KeywordCipher kc = new KeywordCipher("hund");
--- A12/src/hambergerkalaunerorosz/test/TestShiftCipher.java
+++ A12/src/hambergerkalaunerorosz/test/TestShiftCipher.java
@@ -10,33 +10,47 @@ import org.junit.Test;
  * @author Philip Orosz
  *
  */
-@SuppressWarnings("javadoc")
+
 public class TestShiftCipher {
 
-	
+	/**
+	 * Test des Konstruktors mit der Zahl 3
+	 */
 	@Test
 	public void testNormal1() {
 		ShiftCipher sc = new ShiftCipher(1);
 		assertEquals("bcdefghijklmnopqrstuvwxyzäöüßa", sc.getSecretAlphabet());
 	}
 	
+	/**
+	 * Test des Konstruktors mit der Zahl 13
+	 */
 	@Test
 	public void testNormal13(){
 		ShiftCipher sc = new ShiftCipher(13);
 		assertEquals("nopqrstuvwxyzäöüßabcdefghijklm", sc.getSecretAlphabet());
 	}
 	
+	/**
+	 * Test des Konstruktors mit der Zahl 31
+	 */
 	@Test
 	public void testNormal31() {
 		ShiftCipher sc = new ShiftCipher(31);
 		assertEquals("bcdefghijklmnopqrstuvwxyzäöüßa", sc.getSecretAlphabet());
 	}
 	
+	/**
+	 * Testet die Exception falls ein Wert kleiner 0 eingegeben wird
+	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testException(){
 		new ShiftCipher(-3);
 	}
 	
+	/**
+	 * Testet die set Methode 
+	 */
 	@Test
 	public void testSetShiftAmount(){
 		ShiftCipher sc = new ShiftCipher(1);
@@ -44,12 +58,26 @@ public class TestShiftCipher {
 		assertEquals("nopqrstuvwxyzäöüßabcdefghijklm", sc.getSecretAlphabet());
 	}
 	
+	/**
+	 * Testet die Verschlüsselungsmethode
+	 * 
+	 * eingabe: hallo
+	 * verschiebung: 3
+	 * erwartete ausgabe: kdoor
+	 */
 	@Test
 	public void testEncrypt(){
 		ShiftCipher sc = new ShiftCipher(3);
 		assertEquals("kdoor", sc.encrypt("hallo"));
 	}
 	
+	/**
+	 * Testet die Entschlüsselungsmethode
+	 * 
+	 * eingabe: kdoor
+	 * verschiebung: 3
+	 * erwartete ausgabe: hallo
+	 */
 	@Test
 	public void testDecrypt(){
 		ShiftCipher sc = new ShiftCipher(3);
