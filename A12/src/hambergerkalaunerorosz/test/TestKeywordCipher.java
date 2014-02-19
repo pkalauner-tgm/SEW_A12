43427b3a69562357693d33cbfe17ddbe66a6258a
Philip Orosz <porosz@student.tgm.ac.at>
2014-02-19 15:43:18 +0100

    added unittests

--- A12/src/hambergerkalaunerorosz/test/TestKeywordCipher.java
+++ A12/src/hambergerkalaunerorosz/test/TestKeywordCipher.java
@@ -1,6 +1,7 @@
 package hambergerkalaunerorosz.test;
 
 import static org.junit.Assert.*;
+import hambergerkalaunerorosz.KeywordCipher;
 
 import org.junit.Test;
 
@@ -9,11 +10,29 @@ import org.junit.Test;
  * @author Philip Orosz
  *
  */
+@SuppressWarnings("javadoc")
 public class TestKeywordCipher {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testKeywordCipher() {
+		KeywordCipher kc = new KeywordCipher("hund");
+		assertEquals("hundabcefgijklmopqrstvwxyzäöüß", kc.getSecretAlphabet());
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testException(){
+		new KeywordCipher("dieseskeywordistgrößeralsdreissigzeichen");
+	}
+	
+	@Test
+	public void testEncrypt() {
+		KeywordCipher kc = new KeywordCipher("hund");
+		assertEquals("hbba", kc.encrypt("affe"));
+	}
+	
+	@Test
+	public void testDecrypt() {
+		KeywordCipher kc = new KeywordCipher("hund");
+		assertEquals("affe", kc.decrypt("hbba"));
+	}
 }
--- A12/src/hambergerkalaunerorosz/test/TestShiftCipher.java
+++ A12/src/hambergerkalaunerorosz/test/TestShiftCipher.java
@@ -1,6 +1,7 @@
 package hambergerkalaunerorosz.test;
 
 import static org.junit.Assert.*;
+import hambergerkalaunerorosz.ShiftCipher;
 
 import org.junit.Test;
 
@@ -9,11 +10,52 @@ import org.junit.Test;
  * @author Philip Orosz
  *
  */
+@SuppressWarnings("javadoc")
 public class TestShiftCipher {
 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNormal1() {
+		ShiftCipher sc = new ShiftCipher(1);
+		assertEquals("bcdefghijklmnopqrstuvwxyzäöüßa", sc.getSecretAlphabet());
 	}
+	
+	@Test
+	public void testNormal13(){
+		ShiftCipher sc = new ShiftCipher(13);
+		assertEquals("nopqrstuvwxyzäöüßabcdefghijklm", sc.getSecretAlphabet());
+	}
+	
+	@Test
+	public void testNormal31() {
+		ShiftCipher sc = new ShiftCipher(31);
+		assertEquals("bcdefghijklmnopqrstuvwxyzäöüßa", sc.getSecretAlphabet());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testException(){
+		new ShiftCipher(-3);
+	}
+	
+	@Test
+	public void testSetShiftAmount(){
+		ShiftCipher sc = new ShiftCipher(1);
+		sc.setShiftAmount(13);
+		assertEquals("nopqrstuvwxyzäöüßabcdefghijklm", sc.getSecretAlphabet());
+	}
+	
+	@Test
+	public void testEncrypt(){
+		ShiftCipher sc = new ShiftCipher(3);
+		assertEquals("kdoor", sc.encrypt("hallo"));
+	}
+	
+	@Test
+	public void testDecrypt(){
+		ShiftCipher sc = new ShiftCipher(3);
+		assertEquals("hallo", sc.decrypt("kdoor"));
+	}
+	
+	
 
 }
